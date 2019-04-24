a9ea9bff618df988ecfcc80366a18292dc8ee4ed
chris aquino <chris@radishmouse.com>
2019-04-24 11:22:16 -0400

    adds l33tsp34k option for Output component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      inputText: ''
+      inputText: '',
+      names: ["oakley", "milla", "angela"]
     };
   }
 
@@ -17,24 +18,27 @@ class App extends React.Component {
     console.log('Rendering App');
     console.log(`Top of App.render, State is now ${this.state.inputText}`);
 
-    const names = ["oakley", "milla", "angela"];
-
     return (
       <div className="App">
         <header className="App-header">
-        <ul>
+        {/* <ul>
           {
-            names.map(name => <li>{name}</li>)
+            this.state.names.map(name => <li>{name}</li>)
           }
-        </ul>  
+        </ul>   */}
 
-          {/* <input 
+          <input 
             onChange={ this._updateInputText }
-          /> */}
+          />
   
-          {/* <Output
+          <Output
             text={this.state.inputText}
-          /> */}
+          />
+          <Output
+            text={this.state.inputText}
+            transform="l33t"
+          />
+
         </header>
       </div>
     );
